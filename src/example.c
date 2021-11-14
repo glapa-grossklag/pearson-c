d3edbce88cc90bb2fb3066f03b2efdcbf01a9538
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-11-14 10:47:34 -0800

    Make example prettier

--- src/example.c
+++ src/example.c
@@ -10,9 +10,9 @@ int main(void) {
 
     printf("Key: %s\n", key);
 
-    printf("8: %x\n", Pearson8(key, length));
-    printf("16: %x\n", Pearson16(key, length));
-    printf("32: %x\n", Pearson32(key, length));
-    printf("64: %lx\n", Pearson64(key, length));
+    printf("8:      %x\n", Pearson8(key, length));
+    printf("16:     %x\n", Pearson16(key, length));
+    printf("32:     %x\n", Pearson32(key, length));
+    printf("64:     %lx\n", Pearson64(key, length));
     printf("size_t: %zx\n", Pearson(key, length));
 }
